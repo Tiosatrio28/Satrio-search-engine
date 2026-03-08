1235138fa8b9a69d3eb7153e51ed7a8ba8f2e63b
Tiosatrio28 <95740661+Tiosatrio28@users.noreply.github.com>
2026-03-08 19:27:53 +0700

    update all code

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,4 @@
-const searchBtn = document.getElementById('searchBtn');
-const searchInput = document.getElementById('query');
 
-function googleSearch() {
-    const text = searchInput.value;
-    if (text.trim() !== "") {
-        
-        window.location.href = `https://www.google.com/search?q=${encodeURIComponent(text)}`;
-    }
-}
 
 searchBtn.addEventListener('click', (e) => {
     e.preventDefault(); 
@@ -24,13 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuTrigger = document.getElementById('menu-trigger');
     const dropdownMenu = document.getElementById('dropdown-menu');
 
-    // Fungsi klik untuk buka/tutup
+    
     menuTrigger.addEventListener('click', (e) => {
         e.preventDefault();
         dropdownMenu.classList.toggle('show');
     });
 
-    // Menutup menu jika klik di luar area menu
+   
     window.addEventListener('click', (e) => {
         if (!menuTrigger.contains(e.target) && !dropdownMenu.contains(e.target)) {
             dropdownMenu.classList.remove('show');
@@ -43,14 +34,14 @@ const mainMenu = document.getElementById('main-menu');
     const goToAbout = document.getElementById('go-to-about');
     const backToMain = document.getElementById('back-to-main');
 
-    // Klik "Tentang Kami" -> Sembunyikan menu utama, munculkan submenu
+    
     goToAbout.addEventListener('click', (e) => {
         e.preventDefault();
         mainMenu.style.display = 'none';
         aboutSubmenu.style.display = 'block';
     });
 
-    // Klik "Kembali" -> Sembunyikan submenu, munculkan menu utama
+
     backToMain.addEventListener('click', (e) => {
         e.preventDefault();
         aboutSubmenu.style.display = 'none';
